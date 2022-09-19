c33a6ade9c0cf8a8dbf08308625bbdb56cf38453
GeorgijM <108735864+GeorgijM@users.noreply.github.com>
2022-09-19 10:14:15 +0300

    Add files via upload

---
+++ create_db.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+cur = connection.cursor()
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content for the second post')
+            )
+cur.execute("INSERT INTO comments (name, comment) VALUES (?, ?)",
+            ('Ronald', 'How does it works?')
+            )
+cur.execute("INSERT INTO comments (name, comment) VALUES (?, ?)",
+            ('Laura', 'Hi Roland!')
+            )
+connection.commit()
+connection.close()
